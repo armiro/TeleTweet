a2f11f43f373f66daa167313bb6cc5c23e1be2ac
armiro <arman.hgh.1374@gmail.com>
2023-04-01 15:51:33 -0400

    add multiple test functions for authenticate function

--- tests/test_twitter_crawler.py
+++ tests/test_twitter_crawler.py
@@ -1,17 +1,23 @@
 import os
-
 import pytest
 from src.twitter_crawler import *
 
 
-def test_creds_file_exits(creds_path='./twitter_creds.json'):
-    assert os.path.exists(creds_path), "Credentials file does not exist"
+class TestAuthenticate:
 
+    @pytest.fixture()
+    def creds_path(self):
+        return './twitter_creds.json'
 
-def test_authenticate():
-    result = authenticate(creds_path='./twitter_creds.json')
-    assert result.user_agent.find('Tweepy') != -1
+    def test_creds_file_exits(self, creds_path):
+        assert os.path.exists(creds_path), "Credentials file does not exist"
 
+    def test_authenticate_returns_api_object(self, creds_path):
+        api = authenticate(creds_path)
+        assert api is not None, "API object is None"
 
+    def test_authenticate_uses_tweepy(self, creds_path):
+        api = authenticate(creds_path)
+        assert api.user_agent.find('Tweepy') != -1, "API is not properly initiated"
 
 
