f6c0b64fc7915b0aa6c6aacc6d6f4c3a7ea838c8
armiro <arman.hgh.1374@gmail.com>
2023-04-26 17:19:18 -0400

    fix tweet user mention and extract video/gifs from tweets

--- src/twitter_crawler.py
+++ src/twitter_crawler.py
@@ -19,7 +19,7 @@ def authenticate(creds_path):
 
 
 def extract_status_time(tweet):
-    return tweet.created_at.strftime('%a, %d/%B/%Y, %H:%M %Z')
+    return tweet.created_at.strftime('%a, %d/%b/%Y, %H:%M %Z')
 
 
 def extract_tags_and_symbols(tweet, is_retweet):
@@ -36,36 +36,44 @@ def extract_tags_and_symbols(tweet, is_retweet):
 
 def download_media(tweet, media_path, is_retweet):
     if is_retweet:
-        media_files = tweet.retweeted_status.extended_entities.get('media', [])
+        try:
+            media_files = tweet.retweeted_status.extended_entities.get('media', [])
+        except AttributeError:
+            media_files = tweet.retweeted_status.entities.get('media', [])
     else:
-        media_files = tweet.extended_entities.get('media', [])
+        try:
+            media_files = tweet.extended_entities.get('media', [])
+        except AttributeError:
+            media_files = tweet.entities.get('media', [])
+
     for idx, media_file in enumerate(media_files):
-        if media_file.get('type', []) == 'video':
+        media_type = media_file.get('type', [])
+        if media_type == 'video' or media_type == 'animated_gif':
             media_url = media_file['video_info']['variants'][0]['url']
-            print(media_file['video_info']['variants'][0])
         else:
             media_url = media_file['media_url']
         media_name = tweet.id_str + '_' + str(idx) + media_url[media_url.rfind('.'):]
-        # print(media_name)
-        # _ = wget.download(url=media_url, out=media_path + media_name)
-        # print(f'downloaded image no. {idx+1} successfully!')
+        _ = wget.download(url=media_url, out=media_path + media_name)
+        print(f'downloaded media file no. {idx+1} successfully!')
 
-    if not(len(media_files)): print('no downloadable images found!')
-    return (0, None) if not(len(media_files)) else (len(media_files), media_path+media_name)
+    if not(len(media_files)): print('no downloadable media files found!')
+    return (0, None, None) if not(len(media_files)) else (len(media_files), media_type, media_path+media_name)
 
 
 def get_author_name(tweet, is_retweet):
     if is_retweet:
-        original_author_name = tweet.entities.get('user_mentions')[0].get('name')
-        original_author_handle = tweet.entities.get('user_mentions')[0].get('screen_name')
-        return f'RT from: {original_author_name} @{original_author_handle}'
+        author_name = tweet.entities.get('user_mentions')[0].get('name')
+        author_handle = tweet.entities.get('user_mentions')[0].get('screen_name')
+        rt_or_qt = 'QT' if tweet.is_quote_status else 'RT'
+        return f'[{rt_or_qt}] {author_name} <a href="https://twitter.com/{author_handle}">@{author_handle}</a>'
     else:
-        return f'Tweeted by: {tweet.author.name} @{tweet.author.screen_name}'
+        return f'{tweet.author.name} <a href="https://twitter.com/{tweet.author.screen_name}">@{tweet.author.screen_name}</a>'
 
 
 def extract_full_text(tweet, is_retweet):
     return tweet.retweeted_status.full_text if is_retweet else tweet.full_text
 
 
-
+def append_tweet_link(tweet_text):
+    print(tweet_text)
 
