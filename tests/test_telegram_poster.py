cb9ea34d6f36d8b01af06fa48da1f2cb4629c7f8
armiro <arman.hgh.1374@gmail.com>
2023-04-27 20:02:17 -0400

    add new test functions

--- tests/test_telegram_poster.py
+++ tests/test_telegram_poster.py
@@ -3,6 +3,20 @@ import pytest
 from src.telegram_poster import *
 
 
+class TestGetBotUrl:
+
+    @pytest.fixture()
+    def creds_path(self):
+        return './telegram_creds.json'
+
+    def test_creds_file_exits(self, creds_path):
+        assert os.path.exists(creds_path), "Credentials file does not exist"
+
+    def test_url_is_str(self, creds_path):
+        bot_url = get_bot_url(creds_path)
+        assert isinstance(bot_url, str), "Returning URL is not a string"
+
+
 class TestGetBot:
 
     @pytest.fixture()
@@ -11,4 +25,8 @@ class TestGetBot:
 
     def test_creds_file_exits(self, creds_path):
         assert os.path.exists(creds_path), "Credentials file does not exist"
+
+    def test_get_bot_returns_api_object(self, creds_path):
+        bot = get_bot(creds_path)
+        assert bot is not None, "API object is None"
 
